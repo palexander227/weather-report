a4f8fd542f7f1f160c8533b178cc32a86de233da
Mandolin Daze <renormalize@gmail.com>
2021-07-06 20:39:50 -0700

    Created shell html for page and supporting starting css. Basic js variables and api key.

---
+++ script.js
@@ -0,0 +1,7 @@
+var ApiKey = `74daefb956f8b3dbea0d7f059ab98e7a`;
+var storeCity = [];
+var temp = document.getElementById("temp");
+var humidity = document.getElementById("humi");
+var windSpeed = document.getElementById("wind");
+var icon = document.getElementById("icon");
+var cityName = document.getElementById("name");
