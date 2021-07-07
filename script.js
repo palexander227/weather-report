9e4eddb60758d921c0a8f114da42c9f68e7f42f5
Mandolin Daze <renormalize@gmail.com>
2021-07-06 22:49:11 -0700

    Partial fuctionality for access functions to API data and local storage.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var ApiKey = `74daefb956f8b3dbea0d7f059ab98e7a`;
 var storeCity = [];
+if (localStorage.getItem("history")) {
+    storeCity = JSON.parse(localStorage.getItem("history"));
+}
 var temp = document.getElementById("temp");
 var humidity = document.getElementById("humi");
 var windSpeed = document.getElementById("wind");
@@ -7,11 +10,76 @@ var icon = document.getElementById("icon");
 var cityName = document.getElementById("name");
 
 
+var showDetail = () => {
+    //get data from localstorage
+    var weather = JSON.parse(localStorage.getItem("weather"));
+
+    if (weather) {
+        //format date
+        var date = new Date(weather?.date * 1000);
+        var day = date.getDate();
+        var month = date.getMonth() + 1;
+        var year = date.getFullYear();
+        var formattedTime = month + "/" + day + "/" + year;
+
+        cityName.innerHTML = `${weather?.name} ${formattedTime}`;
+        temp.innerHTML = `Temprature: ${weather?.temp} °F`;
+        humidity.innerHTML = `Humidity: ${weather?.humidity} %`;
+        windSpeed.innerHTML = `Wind speed: ${weather?.speed} MPH`;
+        uvi.innerHTML = `UV Index: <span class="${getUviClass(weather?.uvi)}">${weather?.uvi}</span>`;
+        icon.setAttribute(
+            "src",
+            `http://openweathermap.org/img/w/${weather?.icon}.png`
+        );
+        if (weather?.daily) {
+            var forecast5Html = '';
+            for (var i = 0; i < weather.daily.length && i < 5; i++) {
+                date.setDate(date.getDate() + 1);
+                day = date.getDate();
+                month = date.getMonth() + 1;
+                year = date.getFullYear();
+                formattedTime = month + "/" + day + "/" + year;
+                forecast5Html += `<div class="card">
+                <h1>${formattedTime}</h1>
+                <img src="http://openweathermap.org/img/w/${weather.daily[i].weather[0].icon}.png" alt=".." />
+                <p>Temp: ${weather.daily[i].temp.day} F</p>
+                <p>Humidity: ${weather.daily[i].humidity}</p>
+              </div>`;
+            }
+            document.getElementById("forecast-cards").innerHTML = forecast5Html;
+        }
+
+    }
+};
+
+
+
+
+
+
 var fetchWeatherApi = async(city) => {
     var res = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Imperial&appid=${ApiKey}`
     );
     var data = await res.json();
+
+    if (data?.cod == 404) {
+        window.alert("City not found");
+        return false;
+    }
+
+    localStorage.setItem(
+        "weather",
+        JSON.stringify({
+            temp: data.main.temp,
+            humidity: data.main.humidity,
+            speed: data.wind.speed,
+            icon: data.weather[0].icon,
+            date: data.dt,
+            name: data.name,
+        })
+    );
+    showDetail();
 };
 
 var handleSearch = () => {
@@ -23,3 +91,4 @@ var handleSearch = () => {
 
     document.getElementById("city").value = "";
 };
+showDetail();
